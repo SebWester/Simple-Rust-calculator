e2655896867796bc3b43f4ccf9375136548e2c43
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-02-11 23:42:31 +0100

    First code in place

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("A simple Rust calculator");
+
+    let mut op = String::new();    
+    println!("Choose: +, - / or * (Enter quit to exit):");
+
+    loop {        
+        io::stdin()
+            .read_line(&mut op)
+            .expect("Could not read line");
+    
+        let op = op.trim();
+    
+        if op == "quit" {
+            println!("Terminating calculator");
+            break;
+        }
+
+        if op != "+" && op != "-" && op != "/" && op != "*" {
+            println!("Not a valid operator!");
+            return;
+        } 
+        println!("You chose: {}", op);
+    }
+
 }
